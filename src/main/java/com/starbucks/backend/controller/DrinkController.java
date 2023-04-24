fe88c5b57b07be958a147a20288977ea4e5dc989
Ben Hsieh <bqh5026@gmail.com>
2023-04-23 20:35:32 -0500

    Add get mapping for menu items

--- src/main/java/com/starbucks/backend/controller/DrinkController.java
+++ src/main/java/com/starbucks/backend/controller/DrinkController.java
@@ -3,9 +3,12 @@ package com.starbucks.backend.controller;
 import com.starbucks.backend.model.Drink;
 import com.starbucks.backend.repository.DrinkRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DrinkController {
@@ -16,5 +19,10 @@ public class DrinkController {
     @PostMapping("/drink")
     Drink newDrink(@RequestBody Drink newDrink) {
         return drinkRepository.save(newDrink);
+    }
+
+    @GetMapping("/drinks")
+    List<Drink> getAllDrinks() {
+        return drinkRepository.findAll();
     }
 }
