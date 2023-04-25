3e5e42a88d632f2e16a6d84ec0542134eb83a1ea
Ben Hsieh <bqh5026@gmail.com>
2023-04-24 20:58:33 -0500

    Connect Spring Boot back-end to React front-end using @CrossOrigin in controller class

--- src/main/java/com/starbucks/backend/controller/DrinkController.java
+++ src/main/java/com/starbucks/backend/controller/DrinkController.java
@@ -3,14 +3,12 @@ package com.starbucks.backend.controller;
 import com.starbucks.backend.model.Drink;
 import com.starbucks.backend.repository.DrinkRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000/")
 public class DrinkController {
 
     @Autowired
