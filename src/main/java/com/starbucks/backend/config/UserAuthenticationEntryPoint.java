397e77cf6358ed225caea8077a9c9209d38cda32
Ben Hsieh <bqh5026@gmail.com>
2024-03-16 23:12:24 -0500

    Edit UserAuthenticationEntryPoint file

--- src/main/java/com/starbucks/backend/config/UserAuthenticationEntryPoint.java
+++ src/main/java/com/starbucks/backend/config/UserAuthenticationEntryPoint.java
@@ -1,9 +1,12 @@
 package com.starbucks.backend.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.starbucks.backend.dto.ErrorDto;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -17,5 +20,7 @@ public class UserAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+        OBJECT_MAPPER.writeValue(response.getOutputStream(), new ErrorDto("Unauthorized path"));
     }
 }
---
+++ src/main/java/com/starbucks/backend/dto/ErrorDto.java
@@ -0,0 +1,6 @@
+package com.starbucks.backend.dto;
+
+public class ErrorDto {
+    public ErrorDto(String unauthorized_path) {
+    }
+}
