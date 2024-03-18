583618d7f8f728dea46a05b1c1ede510bfbad30b
Ben Hsieh <bqh5026@gmail.com>
2024-03-17 23:56:29 -0500

    Edit ErrorDto file

--- src/main/java/com/starbucks/backend/dto/ErrorDto.java
+++ src/main/java/com/starbucks/backend/dto/ErrorDto.java
@@ -1,6 +1,12 @@
 package com.starbucks.backend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@AllArgsConstructor
+@Builder
+@Data
 public class ErrorDto {
-    public ErrorDto(String unauthorized_path) {
-    }
+    private String message;
 }
