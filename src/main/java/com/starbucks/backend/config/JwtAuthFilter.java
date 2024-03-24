d9c68bb5b58fad270ac56dc2384e6993caa7988d
Ben Hsieh <bqh5026@gmail.com>
2024-03-23 23:34:21 -0500

    Edit JwtAuthFilter file

--- src/main/java/com/starbucks/backend/config/JwtAuthFilter.java
+++ src/main/java/com/starbucks/backend/config/JwtAuthFilter.java
@@ -6,6 +6,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -14,5 +16,22 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (header != null) {
+            String[] elements = header.split(" ");
+
+            if (elements.length == 2 && "Bearer".equals(elements[0])) {
+                try {
+                    SecurityContextHolder.getContext().setAuthentication(
+                            userAuthProvider.validateToken(elements[1]);
+                    );
+                } catch (RuntimeException e) {
+                    SecurityContextHolder.clearContext();
+                    throw e;
+                }
+            }
+        }
+
+        filterChain.doFilter(request, response);
     }
 }
