1e9900b39170744d1f6f840f775942ba3d3a5be8
Ben Hsieh <bqh5026@gmail.com>
2024-03-30 23:44:12 -0500

    Update UserAuthProvider

--- src/main/java/com/starbucks/backend/config/UserAuthProvider.java
+++ src/main/java/com/starbucks/backend/config/UserAuthProvider.java
@@ -1,8 +1,12 @@
 package com.starbucks.backend.config;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @RequiredArgsConstructor
 @Component
@@ -11,6 +15,9 @@ public class UserAuthProvider {
     @Value("${security.jwt.token.secret-key:secret-value")
     private String secretKey;
 
-
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
 
 }
