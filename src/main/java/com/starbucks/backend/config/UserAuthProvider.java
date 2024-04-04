21e6b2dffb036c4b5424bc07cc378b11de44037e
Ben Hsieh <bqh5026@gmail.com>
2024-04-03 23:38:47 -0500

    Add user DTO

--- src/main/java/com/starbucks/backend/config/UserAuthProvider.java
+++ src/main/java/com/starbucks/backend/config/UserAuthProvider.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.starbucks.backend.dto.UserDto;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
---
+++ src/main/java/com/starbucks/backend/dto/UserDto.java
@@ -0,0 +1,4 @@
+package com.starbucks.backend.dto;
+
+public class UserDto {
+}
