e28957de157311b2db4b7a6fef4af9f69d3e10a6
Ben Hsieh <bqh5026@gmail.com>
2024-04-06 23:35:41 -0500

    Update user dto

--- src/main/java/com/starbucks/backend/dto/UserDto.java
+++ src/main/java/com/starbucks/backend/dto/UserDto.java
@@ -10,4 +10,10 @@ import lombok.NoArgsConstructor;
 @Builder
 @Data
 public class UserDto {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String login;
+    private String token;
 }
