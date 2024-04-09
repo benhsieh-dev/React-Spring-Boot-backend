f5a9ce339b9a6abb427cf21bd2d28cf1c528ccde
Ben Hsieh <bqh5026@gmail.com>
2024-04-08 23:37:20 -0500

    Update user service

--- src/main/java/com/starbucks/backend/services/UserService.java
+++ src/main/java/com/starbucks/backend/services/UserService.java
@@ -1,4 +1,19 @@
 package com.starbucks.backend.services;
 
+import com.starbucks.backend.dto.UserDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserDto fingByLogin(String login) {
+        User user = userRepository.findByLogin(login)
+                .orElseThrow( ()-> new AppException("Unknown user", HttpStatus.NOT_FOUND));
+        return userMapper.toUserDto(user);
+    }
 }
