a06200e5a423222f0fa49ee428f0c888592732b8
Ben Hsieh <bqh5026@gmail.com>
2024-04-20 23:45:13 -0500

    Edit user mapper

--- src/main/java/com/starbucks/backend/mappers/UserMapper.java
+++ src/main/java/com/starbucks/backend/mappers/UserMapper.java
@@ -1,4 +1,10 @@
 package com.starbucks.backend.mappers;
 
-public class UserMapper {
+import com.starbucks.backend.dto.UserDto;
+import com.starbucks.backend.entities.User;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    UserDto toUserDto(User user);
 }
