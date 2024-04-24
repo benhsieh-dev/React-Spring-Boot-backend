6ffac0950108b334f5f38cfad7a73d633ea069d0
Ben Hsieh <bqh5026@gmail.com>
2024-04-23 23:25:50 -0500

    Update credentials dto

--- src/main/java/com/starbucks/backend/dto/CredentialsDto.java
+++ src/main/java/com/starbucks/backend/dto/CredentialsDto.java
@@ -1,4 +1,13 @@
 package com.starbucks.backend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
 public class CredentialsDto {
 }
