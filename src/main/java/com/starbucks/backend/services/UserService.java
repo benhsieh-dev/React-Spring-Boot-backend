1688a7aee148ab30bcc8ef4af97db513c5913922
Ben Hsieh <bqh5026@gmail.com>
2024-04-25 23:31:59 -0500

    Update user service

--- src/main/java/com/starbucks/backend/services/UserService.java
+++ src/main/java/com/starbucks/backend/services/UserService.java
@@ -11,6 +11,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.nio.CharBuffer;
+
 @RequiredArgsConstructor
 @Service
 public class UserService {
@@ -28,6 +30,9 @@ public class UserService {
     public UserDto login(CredentialsDto credentialsDto) {
         User user = userRepository.findByLogin(credentialsDto.getLogin())
                 .orElseThrow( ()-> new AppException("Unknown user", HttpStatus.NOT_FOUND));
-
+        if (passwordEncoder.matches(CharBuffer.wrap(credentialsDto.getPassword()), user.getPassword())) {
+            return userMapper.toUserDto(user);
+        }
+        throw new AppException("Invalid password", HttpStatus.BAD_REQUEST);
     }
 }
