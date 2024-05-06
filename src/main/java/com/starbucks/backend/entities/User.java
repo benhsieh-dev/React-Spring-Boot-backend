36611e7c76d725e2dff41fa40d68ecbe2f7e8969
Ben Hsieh <bqh5026@gmail.com>
2024-05-05 23:01:37 -0500

    Edit user entity

--- src/main/java/com/starbucks/backend/entities/User.java
+++ src/main/java/com/starbucks/backend/entities/User.java
@@ -1,4 +1,20 @@
 package com.starbucks.backend.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
 public class User {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String login;
+    private char[] password;
+
 }
