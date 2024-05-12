0fae19859c6d96e854458a3bd83fb4eb1df6948e
Ben Hsieh <bqh5026@gmail.com>
2024-05-11 22:19:51 -0500

    Edit rest exception handler

--- src/main/java/com/starbucks/backend/config/RestExceptionHandler.java
+++ src/main/java/com/starbucks/backend/config/RestExceptionHandler.java
@@ -1,4 +1,20 @@
 package com.starbucks.backend.config;
 
+import com.starbucks.backend.dto.ErrorDto;
+import com.starbucks.backend.exceptions.AppException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class RestExceptionHandler {
+
+    @ExceptionHandler(value={AppException.class})
+    @ResponseBody
+    public ResponseEntity<ErrorDto> handleException(AppException ex) {
+        return ResponseEntity.status(ex.getCode())
+                .body(ErrorDto.builder().message(ex.getMessage()).build());
+    }
+
 }
