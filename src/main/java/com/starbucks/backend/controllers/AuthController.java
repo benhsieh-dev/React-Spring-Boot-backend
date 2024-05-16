d4c7fc5e193c8fb6ff141897ade3e1d1883978d6
Ben Hsieh <bqh5026@gmail.com>
2024-05-15 23:10:19 -0500

    Edit auth controller

--- src/main/java/com/starbucks/backend/controllers/AuthController.java
+++ src/main/java/com/starbucks/backend/controllers/AuthController.java
@@ -1,4 +1,27 @@
 package com.starbucks.backend.controllers;
 
+import com.starbucks.backend.config.UserAuthProvider;
+import com.starbucks.backend.dto.CredentialsDto;
+import com.starbucks.backend.dto.UserDto;
+import com.starbucks.backend.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
 public class AuthController {
+
+    private final UserService userService;
+    private final UserAuthProvider userAuthProvider;
+
+    @PostMapping("/login")
+    public ResponseEntity<UserDto> login(@RequestBody CredentialsDto credentialsDto) {
+        UserDto user = userService.login(credentialsDto);
+
+        user.setToken(userAuthProvider.createToken(user.getLogin()));
+        return ResponseEntity.ok(user);
+    }
 }
