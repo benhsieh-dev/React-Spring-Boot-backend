1c6f76dfaa9e76a59e38c156605303c3ebe98865
Ben Hsieh <bqh5026@gmail.com>
2024-08-05 23:26:20 -0500

    Test configuration files

--- src/main/java/com/starbucks/backend/config/CorsConfiguration.java
+++ src/main/java/com/starbucks/backend/config/CorsConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfiguration {
+public class CorsConfiguration extends org.springframework.web.cors.CorsConfiguration {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
--- src/main/java/com/starbucks/backend/config/SecurityConfiguration.java
+++ src/main/java/com/starbucks/backend/config/SecurityConfiguration.java
@@ -7,10 +7,19 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
@@ -25,18 +34,49 @@ public class SecurityConfiguration {
     }
     private final UserAuthProvider userAuthProvider;
 
+//    @Bean
+//    InMemoryUserDetailsManager users() {
+//        return new InMemoryUserDetailsManager(
+//                User.withUserName("bhsieh")
+//                        .password("{noop}password")
+//                        .roles("USER")
+//                        .build()
+//        );
+//    }
+//    @Bean
+//    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+//
+//        http
+//                .addFilterBefore(new JwtAuthFilter(), BasicAuthenticationFilter.class)
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//
+//        return   http
+//                .csrf().disable()
+//                .cors(Customizer.withDefaults())
+//                .authorizeHttpRequests().anyRequest().permitAll()
+//                .and().build();
+//    }
+
     @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-
-        http
-                .addFilterBefore(new JwtAuthFilter(), BasicAuthenticationFilter.class)
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-        return   http
-                .csrf().disable()
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
                 .cors(Customizer.withDefaults())
-                .authorizeHttpRequests().anyRequest().permitAll()
-                .and().build();
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET"));
+        configuration.setAllowedHeaders(List.of("Authorization"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
