1356a734be58289423cfcce0ec24f5671eff8b15
Ben Hsieh <bqh5026@gmail.com>
2024-08-09 23:49:34 -0500

    Remove commented out code

--- src/main/java/com/starbucks/backend/config/CorsConfiguration.java
+++ src/main/java/com/starbucks/backend/config/CorsConfiguration.java
@@ -20,26 +20,3 @@ public class CorsConfiguration extends org.springframework.web.cors.CorsConfigur
     }
 
 
-//    @Bean
-//    public FilterRegistrationBean corsFilter() {
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        CorsConfiguration config = new CorsConfiguration();
-//        config.setAllowCredentials(true);
-//        config.addAllowedOrigin("http://localhost:3000");
-//        config.setAllowedHeaders(Arrays.asList(
-//                HttpHeaders.AUTHORIZATION,
-//                HttpHeaders.CONTENT_TYPE,
-//                HttpHeaders.ACCEPT));
-//        config.setAllowedMethods(Arrays.asList(
-//                HttpMethod.GET.name(),
-//                HttpMethod.POST.name(),
-//                HttpMethod.PUT.name(),
-//                HttpMethod.DELETE.name()));
-//        config.setMaxAge(MAX_AGE);
-//        source.registerCorsConfiguration("/**", config);
-//        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-//
-//        // should be set order to -100 because we need to CorsFilter before SpringSecurityFilter
-//        bean.setOrder(CORS_FILTER_ORDER);
-//        return bean;
-//    }
