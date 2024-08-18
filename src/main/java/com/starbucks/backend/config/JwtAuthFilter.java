b630748338653d03e3ad6ecfbf4ec4b7c5da8d92
Ben Hsieh <bqh5026@gmail.com>
2024-08-17 23:57:39 -0500

    Remove unused packages

--- src/main/java/com/starbucks/backend/config/JwtAuthFilter.java
+++ src/main/java/com/starbucks/backend/config/JwtAuthFilter.java
@@ -1,13 +1,11 @@
 package com.starbucks.backend.config;
 
-import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
