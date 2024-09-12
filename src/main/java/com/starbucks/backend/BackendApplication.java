6c29e62109657ac5657d63c6c2b2840eb01cab05
Ben Hsieh <bqh5026@gmail.com>
2024-09-11 20:49:14 -0500

    Edit applicaiton file

--- src/main/java/com/starbucks/backend/BackendApplication.java
+++ src/main/java/com/starbucks/backend/BackendApplication.java
@@ -1,14 +1,11 @@
 package com.starbucks.backend;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-@EnableJpaRepositories("com.starbucks.backend.services")
+@EnableJpaRepositories("com.starbucks.backend..*")
 @ComponentScan("com.starbucks.backend.*")
 @EntityScan("com.starbucks.backend.*")
 public class BackendApplication {
