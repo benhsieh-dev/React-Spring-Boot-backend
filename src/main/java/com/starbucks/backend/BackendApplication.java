8c9cbb642b1c58335e1f755a937d277c9f39694d
Ben Hsieh <bqh5026@gmail.com>
2024-09-12 22:27:34 -0500

    Update app

--- src/main/java/com/starbucks/backend/BackendApplication.java
+++ src/main/java/com/starbucks/backend/BackendApplication.java
@@ -1,13 +1,14 @@
 package com.starbucks.backend;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@EnableJpaRepositories("com.starbucks.backend..*")
-@ComponentScan("com.starbucks.backend.*")
-@EntityScan("com.starbucks.backend.*")
+
+@SpringBootApplication
+@RequestMapping("/")
+@RestController
 public class BackendApplication {
 
 	public static void main(String[] args) {
